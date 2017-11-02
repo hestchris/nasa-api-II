7829191ca4440236f83c2ae3ee2b3422242ae60a
Alex Wildman <Alex>
2017-11-02 15:50:56 -0600

    recieving data back from nasa

--- public/main.js
+++ public/main.js
@@ -1,22 +1,19 @@
 $(document).ready(function(){
-
   var newdata;
-
+  let userdate = $('#userdate').val()
   $('#search').on('submit', function(event){
     event.preventDefault()
       var userdate = $('#userdate').val()
-      $.get('/server', {userdate: userdate}, function(res){
+      $.get('/server?userdate=' + userdate, function(res){
+        res = JSON.parse(res)
+        console.log(res);
         newdata = res
+        
         //apend new data to DOM
       })
-    }
-  })
+   })
 })
 
-// $.get('/api?start_date=12-20-12 & end_date=12-22-12')    --client-side
-// fn(body, status, res){}
-// index.html- has a form that user fills out a day
-// main.js - gets the form request and sends a get request it to server.js
-// server.js -
-// $.get('/api?start_date=${userinput}')
-//
+
+
+// body= JSON.parse(body);
--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 var express = require('express')
-
 var app = express()
+var request = require('request')
 
 app.use(express.static('./public'))
 
-app.get('/', function(request, response){
-	response.sendFile('./public/html/index.html', {root: './'})
+app.get('/server', function(req, res){
+  var nasaurl = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${req.query.userdate}&end_date=${req.query.userdate}&api_key=uBbS7vZx5Jtx6pAfsZBVVrMThUPYxy6eMNq7m8iG`;
+  request( nasaurl, function(err, response, body){
+    console.log('fdsfds')
+    res.send(body)
+  })
 })
+
+
+
+//   request({
+//     url: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${req.query.userdate}&end_date=${req.query.userdate}&api_key=uBbS7vZx5Jtx6pAfsZBVVrMThUPYxy6eMNq7m8iG`,
+//     function(err, res, body) {
+//       if(err) {
+//         console.log("There is an error")
+//       }
+//       else {
+//         console.log('body:', body)
+//         res.status(200).send(body);
+//       }
+//     }
+//
+//   })
+// })
 
 app.listen(8080, function(){
 	console.log('This dude is working!')
 })
-// https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-10-18&end_date=2017-10-25&api_key=uBbS7vZx5Jtx6pAfsZBVVrMThUPYxy6eMNq7m8iG
 
 
-res.status(200).send
+
+//`https://api.nasa.gov/neo/rest/v1/feed?start_date=${req.query.userdate}&end_date=${req.query.userdate}&api_key=uBbS7vZx5Jtx6pAfsZBVVrMThUPYxy6eMNq7m8iG`,
+// var nasaurl = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${req.query.userdate}&end_date=${req.query.userdate}&api_key=uBbS7vZx5Jtx6pAfsZBVVrMThUPYxy6eMNq7m8iG`;
+// request(nasaurl, function(err, response, body))
